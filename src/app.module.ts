822a3d898c7695ec23dfe69cf3d7291f8f56bb72
Diego Alejandro Ceron <diego.ceron@palaceresorts.com>
2024-03-01 18:56:37 -0600

    feat: :sparkles: add resume

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { JobModule } from './job/job.module';
+import { ResumeModule } from './resume/resume.module';
 
 @Module({
-  imports: [UserModule, JobModule],
+  imports: [UserModule, JobModule, ResumeModule],
   controllers: [AppController],
   providers: [AppService],
 })
