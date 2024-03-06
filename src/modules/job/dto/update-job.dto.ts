fb425db462f20a5f41f0e22850a5d6d5895fe853
Diego Alejandro Ceron <diego.ceron@palaceresorts.com>
2024-03-05 21:31:22 -0600

    feat: :sparkles: update dto to update job and user

--- src/modules/job/dto/update-job.dto.ts
+++ src/modules/job/dto/update-job.dto.ts
@@ -1,4 +1,23 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateJobDto } from './create-job.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateJobDto extends PartialType(CreateJobDto) {}
+export class UpdateJobDto extends PartialType(CreateJobDto) {
+    @ApiProperty()
+    id: number
+
+    @ApiProperty()
+    title: string
+    
+    @ApiProperty()
+    description: string
+
+    @ApiProperty()
+    monthlySalary: number
+
+    @ApiProperty()
+    exchange: number
+
+    @ApiProperty()
+    benefits: number[]
+}
